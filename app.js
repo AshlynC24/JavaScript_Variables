2682bc46f92bc74123d7bf7aadb4dd0d0324649a
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-03-09 12:01:36 -0500

    app.js is done

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Ashlyn"; //have to declare with const 
+let lastName = "Carmosino";
+var age = 22;
 
 // Exercise 2
+lastName = "Luchini";
+age = 23;
+console.Log('my last name is:' ,{lastName}, 'and my age is:' ,{age})
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995; 
+let isCaseSensitive = true; 
+
+//exerciise 4 
+let price = 19.99;
+let onSale = false; 
+let salePercentage = 15; 
+let stock = 0;
+let inStock = false; 
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722; 
+let bookMark = 456; 
+let hasRead = true; 
+
